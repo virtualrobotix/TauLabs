46eea5f5b115b9cd21cd71ce3069e9efae7afd8a
lilvinz <lilvinz@nscit.com>
2014-02-19 22:09:23 +0100

    pios/common: add support for ChibiOS

--- flight/PiOS/Common/pios_bmp085.c
+++ flight/PiOS/Common/pios_bmp085.c
@@ -6,25 +6,26 @@
   * @brief Hardware functions to deal with the altitude pressure sensor
   * @{
   *
-  * @file       pios_bmp085.c  
+  * @file       pios_bmp085.c
   * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2014
   * @brief      BMP085 Pressure Sensor Routines
   * @see        The GNU Public License (GPL) Version 3
   *
   ******************************************************************************/
-/* 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
@@ -70,7 +71,7 @@ void PIOS_BMP085_EndOfConversion (void)
 #if defined(PIOS_INCLUDE_FREERTOS)
 	/* Yield From ISR if needed */
 	portEND_SWITCHING_ISR(woken ? pdTRUE : pdFALSE);
-#endif
+#endif /* defined(PIOS_INCLUDE_FREERTOS) */
 }
 
 static const struct pios_exti_cfg pios_exti_bmp085_cfg __exti_config = {
